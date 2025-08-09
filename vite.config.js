14495c3713db251df402e629c4ba82cb682ed348
bukh-sal <ssyb2014@gmail.com>
2025-08-09 08:26:52 +0300

    added list url

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        list: resolve(__dirname, 'list.html'),
         at_root: resolve(__dirname, 'at_root.html'),
         wc_w_shadow: resolve(__dirname, 'wc_w_shadow.html'),
         wc_no_shadow: resolve(__dirname, 'wc_no_shadow.html'),
